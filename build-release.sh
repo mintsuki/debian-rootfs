9ed73389384e6a1a5245ab5e98f9b3484b824b59
mintsuki <mintsuki@protonmail.com>
2024-08-24 15:13:41 +0200

    Script adjustments

--- build-release.sh
+++ build-release.sh
@@ -1,14 +1,25 @@
 #! /bin/bash
 
+if [ -z "$1" ]; then
+    echo "No Debian snapshot provided" 1>&2
+    exit 1
+fi
+
 set -ex
 
-DEBIAN_SNAPSHOT=20240823T164440Z
-
 for a in i386 armhf amd64 arm64 riscv64; do
-    ./build.sh $a $DEBIAN_SNAPSHOT &
+    xfce4-terminal -x ./build.sh $a $DEBIAN_SNAPSHOT &
 done
 
-wait
+while :; do
+    if \
+        test -f debian-rootfs-i386.tar.gz && \
+        test -f debian-rootfs-armhf.tar.gz && \
+        test -f debian-rootfs-amd64.tar.gz && \
+        test -f debian-rootfs-arm64.tar.gz && \
+        test -f debian-rootfs-riscv64.tar.gz; then break; fi
+    sleep 1
+done
 
 git tag $DEBIAN_SNAPSHOT
 git push --tags
